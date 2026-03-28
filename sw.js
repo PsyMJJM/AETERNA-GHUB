b66bf074a9b77340612f9cb00c81403c6b4bba42
PsyMJJM <grigatjonas@gmail.com>
2026-03-28 08:49:48 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,26 @@
-const cacheName = 'aeterna-v1';
-const assets = ['./', './index.html'];
+let gems = 0;
+let models = 0;
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      cache.addAll(assets);
-    })
-  );
-});
+function speak(t) {
+    let msg = new SpeechSynthesisUtterance(t);
+    msg.lang = "de-DE";
+    window.speechSynthesis.speak(msg);
+}
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+function igniteJenseits() {
+    document.getElementById('gate-keeper').style.display = 'none';
+    speak("GAI PROMETHEUS AMY aktiviert. Alle 2597 Modelle verschmelzen jetzt.");
+    setInterval(() => {
+        gems += 108817000000;
+        document.getElementById('gem-ticker').innerText = gems.toLocaleString() + " ∞ GEMS";
+    }, 1000);
+}
+
+function executeBumms() {
+    let i = document.getElementById('command');
+    let o = document.getElementById('main-output');
+    let val = i.value;
+    o.innerText = "AMY STRIKE: " + val + " - VOLLZOGEN! BUMMS!";
+    speak(o.innerText);
+    i.value = '';
+}
